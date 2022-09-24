fb47e75c58a35bbd180a97a438fd94ff8c8066c7
Kevin Moyer <kevin7.moyer@gmail.com>
2022-09-24 14:36:50 -0500

    initial react structure created

---
+++ Raid_Roster/src/RaidGroup.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react'
+
+function RaidGroup() {
+
+}
+
+export default RaidGroup
---
+++ Raid_Roster/src/pickCharacters/NonGuildie.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from 'react'
+
+function NonGuildie() {
+
+}
+
+export default NonGuildie
